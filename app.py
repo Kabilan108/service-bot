ee06226ade0c9a85b91fedaa0eddda96cbf25b38
Tony Okeke <tonykabilanokeke@gmail.com>
2023-05-19 20:24:58 -0400

    Create endpoint to bridge whisper with gpt-3.5-turbo

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask, request, send_file, render_template
+"""
+AI Customer Service Bot
+"""
+
+from flask import Flask, request, render_template
+from tempfile import NamedTemporaryFile
+from decouple import config
+import elevenlabs as el
+import openai as ai
+import pickle
 import base64
 import os
 
+
+el.set_api_key(config("ELEVEN_API_KEY"))  # type: ignore
+ai.api_key = config("OPENAI_API_KEY")
+VOICE = "Antoni"
+
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "uploads"
-app.config["RESPONSE_FOLDER"] = "sounds"
+
+
+def ChatCompletion(prompt):
+    completion = ai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a customer service agent. You are helping a customer with a problem. You try your best to provide concise, accurate and helpful answers to the customer's questions.",
+            },
+            {"role": "user", "content": prompt},  # type: ignore
+        ],
+    )
+    return completion.choices[0].message.content  # type: ignore
 
 
 @app.route("/")
@@ -19,11 +45,38 @@ def default_response():
     data = request.json
 
     if "audioMessage" in data:  # type: ignore
-        with open("assets/audio/voice.wav", "rb") as f:
-            audio_base64 = base64.b64encode(f.read()).decode("utf-8")
+        # decode base64 string
+        audio_data = base64.b64decode(data["audioMessage"])  # type: ignore
+
+        # write to temp file
+        with NamedTemporaryFile(suffix=".wav", delete=False) as tf:
+            tf.write(audio_data)
+            tf_path = tf.name
+
+        # transcribe audio
+        with open(tf_path, "rb") as audio:
+            try:
+                transcript = ai.Audio.transcribe("whisper-1", audio, prompt="")
+            except Exception as e:
+                return {"error": str(e)}
+
+        # prompt the assistant
+        completion = ChatCompletion(transcript.text)  # type: ignore
+
+        # generate audio response
+        audio = el.generate(
+            text=completion, voice=VOICE, model="eleven_multilingual_v1"
+        )
+
+        # encode the response
+        audio_base64 = base64.b64encode(audio).decode("utf-8")
         return {"audioResponse": audio_base64}
+
     elif "textMessage" in data:  # type: ignore
-        return {"textResponse": "I am not able to do that yet."}
+        # prompt the assistant
+        completion = ChatCompletion(data["textMessage"])  # type: ignore
+
+        return {"textResponse": completion}
 
     return {"error": "Invalid request. No 'audioMessage' or 'textMessage' found."}
 
