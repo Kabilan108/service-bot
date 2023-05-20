bee15628de3184c4f33ea1673438db38c2820a3b
Tony Okeke <tonykabilanokeke@gmail.com>
2023-05-19 20:24:34 -0400

    Update JS to make chat interface better

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,7 @@ const styles = {
     user: 'p-3 m-2 bg-info text-white rounded-left',
     bot: 'p-3 m-2 bg-success text-white rounded-right'
 }
+const typingIndicator = createTypingIndicator();
 const API = {
     index: "https://0964-68-180-4-143.ngrok-free.app",
     chat: "https://0964-68-180-4-143.ngrok-free.app/api/chat",
@@ -106,6 +107,60 @@ function createAudioPlayer(blobURL, isUser = true) {
     return createChatBox(audioPlayer.outerHTML, isUser);
 }
 
+// Create a typing indicator
+function createTypingIndicator() {
+    const chatBox = document.createElement('div');
+    chatBox.classList.add(styles.bot.split(' ')[0]);
+    chatBox.style.width = '70%';
+    chatBox.style.float = 'left';
+
+    const thinkingDots = document.createElement('div');
+    thinkingDots.classList.add('thinking-dots');
+    thinkingDots.innerHTML = '<span>⬤</span><span>⬤</span><span>⬤</span>';
+
+    chatBox.appendChild(thinkingDots);
+    return chatBox;
+}
+
+// Function to send data to the server
+async function fetchToServer(data) {
+    const response = await fetch(API.default, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+
+    if (!response.ok) {
+        console.error(`HTTP error! status: ${response.status}`)
+    } else {
+        console.log('Success!');
+        const responseData = await response.json();
+        let chatBox;
+
+        if (responseData.textResponse) {
+            chatBox = createChatBox(responseData.textResponse, false);
+        } else if (responseData.audioResponse) {
+            // Convert base64 string to array of integers
+            const byteCharacters = atob(responseData.audioResponse);
+            const byteNumbers = new Array(byteCharacters.length);
+            for (let i = 0; i < byteCharacters.length; i++) {
+                byteNumbers[i] = byteCharacters.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNumbers);
+
+            // Create blob and URL
+            const chatBlob = new Blob([byteArray], { type: 'audio/wav' });
+            const blobURL = window.URL.createObjectURL(chatBlob);
+
+            chatBox = createAudioPlayer(blobURL, false);
+        }
+
+        conversation.removeChild(typingIndicator);
+        conversation.appendChild(chatBox);
+        conversation.scrollTop = conversation.scrollHeight;
+    }
+}
+
 // -------------------------- Event Listeners ---------------------------------
 
 // Toggle recording
@@ -144,50 +199,35 @@ sendButton.addEventListener('click', function () {
         chatBox = createAudioPlayer(audioPlayer.src);
         audioPlayer.remove();
         closeButton.remove();
+
+        // Show chat box and typing indicator
+        userMessage.value = '';
+        conversation.appendChild(chatBox);
+        conversation.appendChild(typingIndicator);
+        conversation.scrollTop = conversation.scrollHeight;
+
+        // Convert blob to base64
+        const reader = new FileReader();
+        reader.readAsDataURL(data.audioMessage);
+        reader.onloadend = function () {
+            data.audioMessage = reader.result.split(',')[1];
+
+            fetchToServer(data);
+        }
     } else {
         // Text message
         data.textMessage = userMessage.value;
         chatBox = createChatBox(data.textMessage);
-    }
 
-    // Append the chat box to the conversation
-    userMessage.value = "";
-    conversation.appendChild(chatBox);
-    conversation.scrollTop = conversation.scrollHeight;
+        // Show chat box and typing indicator
+        userMessage.value = '';
+        conversation.appendChild(chatBox);
+        conversation.appendChild(typingIndicator);
+        conversation.scrollTop = conversation.scrollHeight;
+
+        fetchToServer(data);
+    }
 
     // Clear status message
     statusMessage.innerText = null;
-
-    // Send data to the server
-    fetch(API.chat, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            if (data.textResponse) {
-                chatBox = createChatBox(data.textResponse, false);
-            } else if (data.audioResponse) {
-                // Convert base64 string to array of integers
-                const byteCharacters = atob(data.audioResponse);
-                const byteNumbers = new Array(byteCharacters.length);
-                for (let i = 0; i < byteCharacters.length; i++) {
-                    byteNumbers[i] = byteCharacters.charCodeAt(i);
-                }
-                const byteArray = new Uint8Array(byteNumbers);
-
-                // Create blob and URL
-                const chatBlob = new Blob([byteArray], { type: 'audio/wav' });
-                const blobURL = window.URL.createObjectURL(chatBlob);
-
-                chatBox = createAudioPlayer(blobURL, false);
-            }
-
-            conversation.appendChild(chatBox);
-            conversation.scrollTop = conversation.scrollHeight;
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-})
+});
